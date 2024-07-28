ffcb75d6116a5461c994f1799bc1eb1fc67d00a8
Glenn444 <gmark1586@gmail.com>
2024-07-29 01:44:53 +0300

    C route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,21 +8,26 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """Display Hello HBNB"""
+    """Display 'Hello HBNB!'
+    """
     return "Hello HBNB!"
 
 
 @app.route("/HBHB", strict_slashes=False)
 def hbnb():
-    """Display HBNB"""
+    """Display 'HBNB'
+    """
     return "HBHB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    """Returns given string"""
+    """Returns given string
+    followed by the value of the
+    text variable
+    """
     return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=None)
+    app.run(host='0.0.0.0', port=5000)
