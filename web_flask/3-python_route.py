8cbb67018e3507d04bb07522b43098cf36a14920
Glenn444 <gmark1586@gmail.com>
2024-07-29 01:46:22 +0300

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
-"""Script that starts
-a Flask web application"""
+"""Starts a Flask web application
+"""
 from flask import Flask
 
-app = Flask(__name__)
+if __name__ == '__main__':
+    app = Flask(__name__)
 
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
 
-@app.route("/", strict_slashes=False)
-def hello():
-    """Display Hello HBNB"""
-    return ("Hello HBNB!")
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Display 'HBNB'
+        """
+        return 'HBNB'
 
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """Display “C ” followed by the value of
+        the text variable (replace underscore _
+        symbols with a space)
+        """
+        return 'C ' + text.replace('_', ' ')
 
-@app.route("/HBHB", strict_slashes=False)
-def hbnb():
-    """Display HBNB"""
-    return ("HBHB")
+    @app.route('/python/')
+    @app.route('/python/<text>', strict_slashes=False)
+    def python(text="is cool"):
+        """Display “Python ”, followed by the value of
+        the text variable (replace underscore _
+        symbols with a space )
+        """
+        return 'Python ' + text.replace('_', ' ')
 
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    """Returns given string"""
-    return ("C {}".format(text.replace("_", " ")))
-
-
-@app.route('/python', strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def pythonText(text="is cool"):
-    """display Python followed by the value of the text variable"""
-    return "Python {}".format(text.replace("_", " "))
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=None)
+    app.run('0.0.0.0')
